2a6aa9f93ea2b7c63c6c9e0739a2adfce085cfbd
James Wright <james@jamesswright.co.uk>
2018-11-28 19:00:09 +0000

    Adds test renderer and tests for connectToObservable

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,6 @@ module.exports = {
   },
   testRegex: "src\\/.*\\/__tests__\\/.*\\.test\\.tsx?$",
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  setupTestFrameworkScriptFile: './testSupport/setupEnzyme',
   globals: {
     'ts-jest': {
       tsConfig: './tsconfig.json',
--- src/components/__tests__/connectToObservable.test.tsx
+++ src/components/__tests__/connectToObservable.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
-import connectToObservable from '../connectToObservable';
 import { empty } from 'rxjs';
+import { createQueryableComponent, render, queryProps } from './testRenderer';
+import connectToObservable from '../connectToObservable';
 
 interface State {
   foo: string;
@@ -13,23 +13,43 @@ interface OwnProps {
   qux: string;
 }
 
-const MyComponent = () => <div />;
+const defaultState = {
+  foo: 'lol',
+  bar: 'rofl',
+};
+
+// TODO: test observable emission when react-test-renderer supports hooks
 
 describe('connectToObservable', () => {
   it('should render the wrapped component with defaultState as props', () => {
-    const defaultState = {
-      foo: 'lol',
-      bar: 'rofl',
-    };
+    const MyComponent = createQueryableComponent<State>();
 
     const WrappedComponent = connectToObservable<State>(
       empty(),
       defaultState,
     )(MyComponent);
 
-    const rendered = shallow(<WrappedComponent />);
+    const renderedElement = render(<WrappedComponent />);
+    const props = queryProps<State>(renderedElement);
 
-    expect(rendered.prop('foo')).toEqual('lol');
-    expect(rendered.prop('bar')).toEqual('rofl');
+    expect(props.foo).toEqual('lol');
+    expect(props.bar).toEqual('rofl');
+  });
+
+  it('should proxy its own props to the wrapped component', () => {
+    const MyComponent = createQueryableComponent<State & OwnProps>();
+
+    const WrappedComponent = connectToObservable<State, OwnProps>(
+      empty(),
+      defaultState,
+    )(MyComponent);
+
+    const renderedElement = render(<WrappedComponent baz="one" qux="two" />);
+    const props = queryProps<State & OwnProps>(renderedElement);
+
+    expect(props.foo).toEqual('lol');
+    expect(props.bar).toEqual('rofl');
+    expect(props.baz).toEqual('one');
+    expect(props.qux).toEqual('two');
   });
 });
